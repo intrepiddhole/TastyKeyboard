180a814beeb7458825194450f9cc80c6829972f1
Wang Xing <callme0802@gmail.com>
2015-12-30 05:40:56 +0100

    First Version

---
+++ HostingApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  TransliteratingKeyboard
+//
+//  Created by Alexei Baboulevitch on 6/9/14.
+//  Copyright (c) 2014 Apple. All rights reserved.
+//
+
+import UIKit
+
+class HostingAppViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
+
